3f809ec22a0f7869153bc8cff0f29d50751b87a9
cola <mgjeong@yonsei.ac.kr>
2022-01-27 13:36:51 -0600

    sorry mistake

--- implementation/1157.py
+++ implementation/1157.py
@@ -1,6 +1,7 @@
 word = input().upper()
-word.sort()
+
 S = set(list(word)) # alphabet 하나씩만
+S = list(S)
 
 lst = []
 for w in S:
@@ -8,9 +9,14 @@ for w in S:
     for a in word:
         if a == w:
             cnt += 1
-        lst.append(cnt)
-max = max(lst)
-print(max)
-        
-        
+    lst.append(cnt)
+    
+max = max(lst) #가장 많이 나온 횟수
+idx = lst.index(max)
+del lst[lst.index(max)]
+if max in lst:
+    print('?')
+else:
+    print(S[idx])
 
+# 집합은 indexing slicing 불가 => set 후 다시 list 만들어야 접근 가능
