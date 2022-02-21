e754dfe20051979aa423028b6098ef4976ca0572
cola <mgjeong@yonsei.ac.kr>
2022-02-20 20:46:19 -0600

    feb 20, 2022 remote!

--- C++/basics/2775_house.cpp
+++ C++/basics/2775_house.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-// k 층 n 호 = k-1 층의 1호부터 n 호까지의 합
-
-// = k-1 층의 n 호와 k 층의 n-1 호의 합과 같습니다.
+// k 층 n 호 = k-1 층의 1호부터 n 호까지의 합 = k-1 층의 n 호와 k 층의 n-1 호의 합
 
 int fillFloor(int k, int n) {
     if (n==1) return 1;
---
+++ bruteforcing/1107remote.py
@@ -0,0 +1,20 @@
+# bruteforce => search for every single possible cases.
+import sys
+input = sys.stdin.readline
+N = int(input()) # goal
+M = int(input()) # number of broken buttons
+broken = list(map(int, input().split()))
+
+min_cnt = abs(100 - N)
+
+for nums in range(1000001): # also have to check above the goal (500000 * 2)
+    nums = str(nums)
+    
+    for j in range(len(nums)):
+        if int(nums[j]) in broken: # if there is broken number
+            break
+
+        elif j == len(nums) - 1: # there aren't broken numbers => candidate for min count
+            min_cnt = min(min_cnt, abs(int(nums) - N) + len(nums))
+
+print(min_cnt)
