b2163c7629230bc98507c475f698b713c96a38c4
cola <mgjeong@yonsei.ac.kr>
2022-04-23 22:02:37 -0500

    tomato including 3d

--- dfs_bfs/7576_tomato.py
+++ dfs_bfs/7576_tomato.py
@@ -1,26 +1,48 @@
-# working
-
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-M, N = map(int, input().split()) # num col, num row # M x N matrix
+M, N = map(int, input().split()) # num col, num row
+n, m = N, M # n x m matrix
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
-temp = []
-for _ in range(N):
-    temp.append(list(map(int, input().split())))
-    
-graph = [[0]*N for _ in range(M)] # Generate M x N matrix
+# 1 인 지점 (virus 개념)을 모두 넣고 시작. 한 번에 퍼지기 때문
+queue = deque()
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append((i,j))
 
-for i in range(N):
-    for j in range(M):
-        if temp[i][j] != -1:
-            graph[i][j] = 1
+day = -1          # 전부 1이면 밑에 다 돌고 -1 + 1 = 0 return # 뭐라도 큐에 있으면 +1 되서 마지막 날까지 카운트 됨
+def bfs(graph):
+    global day
+    while queue:
+        day += 1
+        for _ in range(len(queue)): # 1로 시작하는 (감염된 애들 수 만큼)
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if graph[nx][ny] == -1:
+                    continue
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = 1 # 새로 전파
+                    queue.append((nx,ny)) # 새로 익은 애들을 전부 큐에 넣어줌  
+    return graph
 
-
+box = bfs(graph)
+for row in box:
+    if 0 in row:
+        print(-1)
+        break
+else:
+    print(day)
         
-print(temp)
-print(graph)
+        
